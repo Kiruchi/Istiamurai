244eab87d9b2daa882212cb2ab0d95b7888389e6
Pierre Martin <pierre.martin49@gmail.com>
2016-01-20 15:48:10 +0100

    Update UI + GameLogic script

---
+++ Istiamurai/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject score;
+    private bool inGame;
+    private bool isShowing;
+
+	// Use this for initialization
+	void Start () {
+        inGame = false;
+        isShowing = true;
+        menu.SetActive(isShowing);
+        score.SetActive(!isShowing);
+	}
+
+    void click_play() {
+        Debug.Log("Click play");
+        inGame = true;
+        isShowing = false;
+        menu.SetActive(isShowing);
+        score.SetActive(!isShowing);
+        // Play
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && inGame)
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+            score.SetActive(!isShowing);
+        }
+    }
+}
