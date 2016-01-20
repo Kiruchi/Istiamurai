d2e0b840a01da03f2723db4cbedfd9c9a14fd4e0
Pierre Michel <michel.pierre.35@gmail.com>
2016-01-20 14:12:35 +0100

    Ajout script emition d'objets.

---
+++ Istiamurai/Assets/Scripts/CutenessEmitter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutenessEmitter : MonoBehaviour
+{
+    [Range(0.1f, 5.0f)]
+    public float timeBetweenEmitions = 2.0f;
+
+    private float lastEmitionTime = 0;
+	
+	void Update()
+    {
+        //If time since last emition long enough, ...
+	    if(Time.time >= lastEmitionTime + timeBetweenEmitions) {
+            //Get a random throwable item.
+            GameObject item = null;
+            //Emit this item.
+            GameObject newItem = GameObject.Instantiate(item, transform.position, transform.rotation) as GameObject;
+            //Modify the attributes of the new item.
+            //TODO
+        }
+	}
+}
